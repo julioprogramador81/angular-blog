d10c76cf33f59a8314806bd10118410bfb4cb7f5
julioprogramador81 <julioprogramador81@gmail.com>
2024-02-02 14:44:50 -0300

    Pequenas mudanças, introduzi pagina 3 e 4 no dataFake e algus ajustis css da pagina

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -2,17 +2,34 @@ export const dataFake = [
     {
         "id":"1",
         "title": "DE FERRO EM 3D",
-        "description": "osanuncia novo filme homen de ferro que estara nos cinemas e na tv em breve.",
+        "description": "Desde que Tony Stark nos deixou ao final de Vingadores Ultimato, o Universo Cinematográfico da Marvel nunca mais foi o mesmo. E parece que esta mudança na atmosfera dos filmes foi percebida também pelos grandes executivos da Disney. Como parte da imensa reestruturação que vem sendo feita no Marvel Studios, o playboy filantropo vai retornar aos filmes da Marvel. ",
         "photoCover": "https://imageio.forbes.com/specials-images/imageserve/63600220295903240c876e05/0x0.jpg?format=jpg&height=600&width=1200&fit=bounds"
 
     },
     {
         "id":"2",
-        "title": "saiu novo filme",
-        "description": "Nova serie anunciada pela marvel.",
+        "title": "Sobre as series e filmes da marvel!",
+        "description": "No mundo geek, ao falarmos em crossover, não tem para ninguém: a Marvel é campeã no quesito 'conectar' personagens e tramas em seus filmes e séries. Desde 2008, quando lançou o primeiro Homem de Ferro, o estúdio se preocupa em entrelaçar todas as produções no que é conhecido como Marvel Cinematic Universe (Universo Cinematográfico da Marvel, em português).",
         "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkcdbUQA6p2bQd1DjyN1Kzx9QpMFlLuUd9NACy-TKDeQk48aep_M4QMzNGSR9mpnASZvc&usqp=CAU"
 
+    },
+    {
+        "id":"3",
+        "title": "Curiosidade sobre filme pantera negra!",
+        "description": "O diretor Ryan Coogler afirmou que em memória ao legado de Chadwick, o seu personagem não seria recriado digitalmente. A associação da morte do ator e de seu personagem é real. No final, os personagens usaram branco, no funeral, como cor representante do luto, seguindo a tradição de Wakanda ao perderem entes queridos.!",
+        "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1SFBuA_8Sktlyyi3zCqJ00OGgK7b7x87B-Q&usqp=CAU"
+
+    },
+
+    {
+        "id":"4",
+        "title": "Infopédia do X-MEN",
+        "description": "Série de banda desenhada da Marvel Comics, criada nos Estados Unidos da América (EUA) em 1963, por Stan Lee (argumento) e Jack Kirby (desenho). X-Men é o nome de um grupo constituído por jovens mutantes, cada qual exímio num poder muito específico.",
+        "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbH2M72swgBUSqEZQQJnQA0MSsII8oW9JXS_E0aFVYNXQSr6E77L3lK5eWm51MDkJxJ34&usqp=CAU"
+
+
     }
+    
 ]
     
   
